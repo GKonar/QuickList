1b0cb5f8e0e7875a3de5710f4bb84a06b3181f82
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-04-17 16:56:15 +0200

    Add app theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import appTheme from './themes/appTheme';
 
 function App() {
   return (
+    <ThemeProvider theme={appTheme}>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/appTheme.js
@@ -0,0 +1,54 @@
+const theme = {
+  padding: {
+    small: '8px',
+    regular: '12px',
+    default: '16px',
+    between: '24px',
+    medium: '32px',
+    large: '48px',
+    xlarge: '64px',
+  },
+
+  margin: {
+    small: '8px',
+    regular: '12px',
+    default: '16px',
+    between: '24px',
+    medium: '32px',
+    large: '48px',
+    xlarge: '64px',
+  },
+
+  borderRadius: {
+    small: '3px',
+    default: '5px'
+  },
+  indentation: 50,
+
+  color: {
+    main: '#F59486',
+    primary: '#2f3542',
+    secondary: '#f1f2f6',
+    white: '#FFFFFF',
+    error: '#e74c3c',
+    success: '#32ff7e'
+  },
+
+  fontSize: {
+    huge: '50px',
+    bigger: '30px',
+    big: '24px',
+    medium: '20px',
+    default: '16px',
+    readable: '14px',
+    secondary: '12px',
+    tiny: '10px',
+  },
+
+  input: {
+    border: '1px solid #dedede',
+    borderRadius: '4px',
+  },
+};
+
+export default theme;
