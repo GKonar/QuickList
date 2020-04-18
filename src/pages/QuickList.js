4fdd976a6f3c8a25b141876661e4f6e38b5fc1e1
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-04-18 23:29:52 +0200

    Add background for light mode

--- src/pages/QuickList.js
+++ src/pages/QuickList.js
@@ -4,9 +4,16 @@ import styled from 'styled-components';
 import Navbar from '../components/Navbar';
 import AddItemForm from '../components/AddItemForm';
 
+// import light from '../assets/backgrounds/what-the-hex.png';
+// import dark from '../assets/backgrounds/what-the-hex-dark.png';
+
+import light1 from '../assets/backgrounds/beanstalk.png';
+import dark1 from '../assets/backgrounds/beanstalk-dark.png';
+
+
 const ListWrap = styled.section`
   height: 100vh;
-  /* background-color: #4bcffa; */
+  background-image: url(${light1}); /* for the first mode */
 `
 const InputWrap = styled.div`
   display: flex;
