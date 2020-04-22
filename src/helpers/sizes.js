2ceb350da1e3d53905777ab7b7b3f768b6f61bca
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2020-04-22 21:23:49 +0200

    Add responsive breakpoints

---
+++ src/helpers/sizes.js
@@ -0,0 +1,13 @@
+// Breakpoints
+export const sizes = {
+  mobileS: `@media(max-width: 350px)`,
+  mobileM: `@media(max-width: 375px)`,
+  mobileL: `@media(max-width: 425px)`,
+  tablet: `@media(max-width: 768px)`,
+  laptopS: `@media(max-width: 1024px)`,
+  laptopM: `@media(max-width: 1280px)`,
+  laptopL: `@media(max-width: 1440px)`,
+  desktopM: `@media(max-width: 1600px)`,
+  desktopL: `@media(max-width: 2560px)`,
+}
+
